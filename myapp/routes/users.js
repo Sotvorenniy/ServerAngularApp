76d0495cf94ddd2931463d1205cb30bab72afe10
Sotvorenniy <natasha.natidze@gmail.com>
2020-09-07 11:54:28 +0300

    add all files

---
+++ myapp/routes/users.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const uuid = require('uuid').v4;
+const users = [];
+/* GET users listing. */
+router.get('/', (req, res) => {
+  res.send(users);
+});
+
+router.post('/', (req, res) => {
+    console.log(req.body);
+    const user = {
+        id: uuid(),
+        ...req.body
+    };
+    console.log('user', user);
+    users.push(user);
+    res.send({ email: user.email, login: user.login, name: user.name});
+});
+
+module.exports = router;
