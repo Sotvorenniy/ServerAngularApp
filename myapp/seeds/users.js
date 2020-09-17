7bfc066e513bb68d25d75ab1e75e2bd51671e0b3
Sotvorenniy <natasha.natidze@gmail.com>
2020-09-17 18:04:05 +0300

    add put method, update seeds: default completed false

--- myapp/seeds/users.js
+++ myapp/seeds/users.js
@@ -7,8 +7,8 @@ exports.seed = function(knex) {
     .then(async  function () {
       // Inserts seed entries
       return knex('users').insert([
-        {login: 'Sotvorenniy', name: 'Natasha', email: "test@m.ru", token: crypto.randomBytes(64).toString("hex"), password: await bcrypt.hash('123456', 10),},
-        {login: 'Kri', name: 'Anna', email: "testtwo@mail.ru", token: crypto.randomBytes(64).toString("hex"),password: await bcrypt.hash('123456', 10),},
+        {login: 'Sotvorenniy', name: 'Natasha', completed: false, email: "test@m.ru", token: crypto.randomBytes(64).toString("hex"), password: await bcrypt.hash('123456', 10),},
+        {login: 'Kri', name: 'Anna', completed: false, email: "testtwo@mail.ru", token: crypto.randomBytes(64).toString("hex"),password: await bcrypt.hash('123456', 10),},
       ]);
     });
 };
